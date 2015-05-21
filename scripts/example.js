70a32d560bb0db655a1198b82fd963b31ecc4bc1
Cherry Ng <cherry92@gmail.com>
2015-05-22 07:55:15 +0800

    add make me a sandwich command

--- scripts/example.js
+++ scripts/example.js
@@ -4,6 +4,23 @@ module.exports = function (robot) {
 
     // say 'hello world' when hear someone say hello
     robot.hear(/hello/i, function (res) {
+        // send a message to the room receive this message
         res.send('hello world')
     })
+
+    /*
+    // will respond to 'hubot make me a sandwich'
+    robot.respond(/make me a sandwich/i, function (res) {
+        // using 'reply' will include the name of sender
+        res.reply('do it yourself')
+    })
+    */
+
+    /*
+    // alternate response when hearing sudo together
+    robot.respond(/sudo make me a sandwich/i, function (res) {
+        res.reply('ok')
+    })
+    */
+
 }
