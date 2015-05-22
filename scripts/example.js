e5fe081a2891ad933367116e17a3b630d416a5fa
Cherry Ng <cherry92@gmail.com>
2015-05-22 08:04:28 +0800

    use string instead of regex for matching

--- scripts/example.js
+++ scripts/example.js
@@ -3,14 +3,14 @@
 module.exports = function (robot) {
 
     // say 'hello world' when hear someone say hello
-    robot.hear(/hello/i, function (res) {
+    robot.hear('hello', function (res) {
         // send a message to the room receive this message
         res.send('world')
     })
 
     /*
     // will respond to 'hubot make me a sandwich'
-    robot.respond(/make me a sandwich/i, function (res) {
+    robot.respond('make me a sandwich', function (res) {
         // using 'reply' will include the name of sender
         res.reply('do it yourself')
     })
@@ -18,7 +18,7 @@ module.exports = function (robot) {
 
     /*
     // alternate response when hearing sudo together
-    robot.respond(/sudo make me a sandwich/i, function (res) {
+    robot.respond('sudo make me a sandwich', function (res) {
         res.reply('okay')
     })
     */
