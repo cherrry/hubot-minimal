d6687dc36ab59092782cf91c9423bb879a481fd3
Cherry Ng <cherry92@gmail.com>
2015-05-23 11:10:50 +0800

    update example to use regex

--- scripts/example.js
+++ scripts/example.js
@@ -3,14 +3,14 @@
 module.exports = function (robot) {
 
     // say 'hello world' when hear someone say hello
-    robot.hear('hello', function (res) {
+    robot.hear(/hello/i, function (res) {
         // send a message to the room receive this message
         res.send('world')
     })
 
     /*
     // will respond to 'hubot make me a sandwich'
-    robot.respond('make me a sandwich', function (res) {
+    robot.respond(/make me a sandwich/i, function (res) {
         // using 'reply' will include the name of sender
         res.reply('do it yourself')
     })
@@ -18,7 +18,7 @@ module.exports = function (robot) {
 
     /*
     // alternate response when hearing sudo together
-    robot.respond('sudo make me a sandwich', function (res) {
+    robot.respond(/sudo make me a sandwich/i, function (res) {
         res.reply('okay')
     })
     */
@@ -31,7 +31,7 @@ module.exports = function (robot) {
         '...',
         'I refuse :angry:'
     ]
-    robot.respond('say something', function (res) {
+    robot.respond(/say something/i, function (res) {
         // choose on of the response from `randomResponse`
         var message = res.random(randomResponse)
         // send it back
